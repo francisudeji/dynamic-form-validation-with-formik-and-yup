be55c50bd25b8acbd0c6c025d4902df03c19abd6
francisudeji <francis.udejiofficial@gmail.com>
2020-05-01 08:29:45 +0100

    Finish validation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import './App.css';
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: async (value) => {
+      console.log(value);
+    },
+    validationSchema: () => {
+      return Yup.lazy(({ username }) => {
+        return Yup.object().shape({
+          username: String(username).match(/[a-z]/gi)
+            ? Yup.string().email().required('Email or phone is required')
+            : String(username).match(/[0-9]@/)
+            ? Yup.string().email().required('Email or phone is required')
+            : Yup.string()
+                .matches(
+                  /^(090|080|070|081)[0-9]{8}/,
+                  'Phone number must be start with 090, 080, 081, 070, and be 11 digits.'
+                )
+                .required('Phone number is required'),
+          password: Yup.string()
+            .matches(
+              /[0-9][a-zA-Z]/,
+              'Password must contain lowercase letters, uppercase letters and numbers'
+            )
+            .min(6, 'Password must be at least 6 characters')
+            .required('Password is required'),
+        });
+      });
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={formik.handleSubmit}>
+        <h2>Welcome back</h2>
+        <div>
+          <label htmlFor='username'>Email or phone</label>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            placeholder='Enter email or phone'
+            autoComplete='username'
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.username && formik.errors.username ? (
+            <span>{formik.errors.username}</span>
+          ) : null}
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            placeholder='Enter your password'
+            autoComplete='current-password'
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <span>{formik.errors.password}</span>
+          ) : null}
+        </div>
+
+        <button>Login</button>
+      </form>
     </div>
   );
 }
